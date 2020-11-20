044224ff1a92bd53342483a70ce8847f7f431ed0
Savanna Denega Machado <savanna.denega@hotmail.com>
2020-11-20 15:17:13 -0300

    Adição feature geração de predição

--- src/predicoescontextos/predicoes_contextos.py
+++ src/predicoescontextos/predicoes_contextos.py
@@ -2,37 +2,66 @@
 # https://nirpyresearch.com/partial-least-squares-regression-python/
 # https://scikit-learn.org/stable/modules/generated/sklearn.cross_decomposition.PLSRegression.html
 # https://drive.google.com/file/d/1Y1iG4qDCuMcNp-aZgUFY5LPdVQeM2XiQ/view
+# https://nirpyresearch.com/pls-discriminant-analysis-binary-classification-python/
 
-from sys import stdout
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 from sklearn.cross_decomposition import PLSRegression
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import KFold, cross_val_predict, train_test_split
+from sklearn.metrics import accuracy_score
+import os
+from pathlib import Path
+import csv
 
-# Define PLS object
-# TODO ver necessidade 2 parametro
-pls = PLSRegression(n_components=5)
+global pls_binary
 
-# Fit
-# TODO adicionar dcaredatasetsimulator gerado pelo gerador de dcaredatasetsimulator
-data = pd.read_csv('/dcaredatasetsimulator/peach_spectra_brix.csv')
+def train():
+    # Next, we load the data, extract the first and the last label, and do some cleaning and preprocessing steps
+    # Load data into a Pandas dataframe
+    dirname = Path(__file__).parent.parent
+    filepath = os.path.join(dirname, 'db/milk-powder.csv')
+    data = pd.read_csv(filepath)
+    # Extract first and last label in a new dataframe
+    binary_data = data[(data['labels'] == 5) | (data['labels'] == 6)]
+    # Read data into a numpy array and apply simple smoothing
+    X_binary = savgol_filter(binary_data.values[:, 2:], 15, polyorder=3, deriv=0)
+    # Read categorical variables
+    y_binary = binary_data["labels"].values
+    # Map variables to 0 and 1
+    y_binary = (y_binary == 6).astype('uint8')
 
-# Get reference values
-# TODO mudar nomes das colunas e linhas para leitura
-Y = data['Brix'].values
-# Get spectra
-X = data.drop(['Brix'], axis=1).values
+    # Now it’s time for the actual PLS decomposition, which is as simple as
+    # Define the PLS regression object
+    # pls_binary = PLSRegression(n_components=2)
+    # Fit and transform the data
+    X_pls = pls_binary.fit_transform(X_binary, y_binary)[0]
+    return X_binary, y_binary
 
-pls.fit(X, Y)
 
-# Cross-validation
-y_cv = cross_val_predict(pls, X, Y, cv=10)
+def pls_da(X_train, y_train, X_test):
+    # Define the PLS object for binary classification
+    plsda = PLSRegression(n_components=2)
 
-# Calculate scores
-# TODO mudar score para retorno em binario
-score = r2_score(Y, y_cv)
-mse = mean_squared_error(Y, y_cv)
+    # Fit the training set
+    plsda.fit(X_train, y_train)
+
+    # Binary prediction on the test set, done with thresholding
+    binary_prediction = (pls_binary.predict(X_test)[:, 0] > 0.5).astype('uint8')
+
+    return binary_prediction
+
+
+# Execucao
+pls_binary = PLSRegression(n_components=2)
+X_binary, y_binary = train()
+accuracy = []
+cval = KFold(n_splits=10, shuffle=True, random_state=19)
+for train, test in cval.split(X_binary):
+    y_pred = pls_da(X_binary[train, :], y_binary[train], X_binary[test, :])
+
+    accuracy.append(accuracy_score(y_binary[test], y_pred))
+print("Average accuracy on 10 splits: ", np.array(accuracy).mean())
+
 
